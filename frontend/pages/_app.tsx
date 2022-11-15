212bab0112850d68aa04c28f67c81377b81752de
김도현 <swputer@gmail.com>
2022-11-15 14:32:38 +0900

    build: styled-components 설정 - #11

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
+import GlobalStyle from '../styles/GlobalStyle';
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
 }
---
+++ frontend/styles/GlobalStyle.ts
@@ -0,0 +1,7 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyle = createGlobalStyle`
+${reset}`;
+
+export default GlobalStyle;
