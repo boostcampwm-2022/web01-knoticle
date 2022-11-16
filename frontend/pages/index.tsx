fd344035266b84af9dec641b74cf87f9749b344e
MinHK4 <dzf1224@naver.com>
2022-11-16 16:29:32 +0900

    feat: 임시 래퍼 생성 - #5

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,21 +1,24 @@
 import styled from 'styled-components';
 
-const Test = styled.div`
-  width: 100px;
-  height: 100px;
-  font-size: 5em;
-  background-color: red;
+const Page = styled.div`
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
-const Logo = styled.div`
-  font-family: 'Sofia';
+const TempWrapper = styled.div`
+  width: 400px;
+  height: 400px;
+  background-color: red;
 `;
 
 export default function AAA() {
   return (
-    <Test>
-      <Logo>Knoticle</Logo>
-      Hi
-    </Test>
+    <Page>
+      <TempWrapper>Login</TempWrapper>
+    </Page>
   );
 }
