8af55696ea7167e0fa2d6d2e3757c6e9fb651036
김도현 <swputer@gmail.com>
2022-11-16 20:40:01 +0900

    feat: JWT 발급 함수 추가 - #7

--- backend/src/apis/auth/auth.service.ts
+++ backend/src/apis/auth/auth.service.ts
@@ -1,3 +1,5 @@
+import jwt from 'jsonwebtoken';
+
 import { prisma } from '../../config/orm.config';
 
 export const getSignInResult = async (username: string, password: string) => {
@@ -13,15 +15,18 @@ export const getSignInResult = async (username: string, password: string) => {
     },
   });
 
-  if (!user) {
-    // 회원가입이 되어있지 않은 경우
-    throw new Error();
-  }
+  // 회원가입이 되어있지 않은 경우
+  if (!user) throw new Error();
 
-  if (user.password !== password) {
-    // 비밀번호가 불일치하는 경우
-    throw new Error();
-  }
+  // 비밀번호가 불일치하는 경우
+  if (user.password !== password) throw new Error();
+
+  const accessToken = generateJWT('3h', { id: user.id });
+  const refreshToken = generateJWT('7d');
 
   return user;
+};
+
+const generateJWT = (expiresIn: '3h' | '7d', payload: { id?: number } = {}) => {
+  return jwt.sign(payload, process.env.JWT_SECRET_KEY, { expiresIn });
 };
