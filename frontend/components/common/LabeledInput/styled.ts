44e3d94c70e5022d3e7eb4afe505622f164f64d8
김도현 <swputer@gmail.com>
2022-11-17 20:05:45 +0900

    feat: LabeledInput 컴포넌트 레이아웃 작업 - #4

---
+++ frontend/components/common/LabeledInput/styled.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const LabeledInputWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
+
+export const Input = styled.input`
+  padding: 16px;
+  width: 100%;
+  box-sizing: border-box;
+  font-size: 16px;
+  border: 1px solid var(--grey-02-color);
+  border-radius: 10px;
+`;
