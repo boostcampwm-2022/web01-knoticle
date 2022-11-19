e5a858e4315bce2cc8f9fa24320c346beafe07ad
김도현 <swputer@gmail.com>
2022-11-19 15:57:30 +0900

    feat: 에디터 페이지 Editor 컴포넌트 추가 - #24

--- frontend/pages/editor.tsx
+++ frontend/pages/editor.tsx
@@ -1,5 +1,11 @@
 import EditBar from '../components/EditBar';
+import Editor from '../components/Editor';
 
 export default function EditorPage() {
-  return <EditBar />;
+  return (
+    <>
+      <EditBar />
+      <Editor />
+    </>
+  );
 }
