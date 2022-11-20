bd1cf50e030c0171bc1726fa0a2c1618cb87ee81
dahyeon405 <dahyeon405@naver.com>
2022-11-20 16:48:27 +0900

    feat: 책 컴포넌트 UI 작업 - #23

--- frontend/styles/layout.ts
+++ frontend/styles/layout.ts
@@ -1,9 +1,17 @@
 import styled from 'styled-components';
 
-const FlexCenter = styled.div`
+export const FlexCenter = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 
-export default FlexCenter;
+export const FlexColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const FlexSpaceBetween = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
