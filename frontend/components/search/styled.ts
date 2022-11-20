c4422147d65dd37f7f36071f83673470bd7b3693
김도현 <swputer@gmail.com>
2022-11-20 17:51:05 +0900

    chore: search 폴더 이름 대소문자 변경

---
+++ frontend/components/search/styled.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+// eslint-disable-next-line import/prefer-default-export
+export const SearchArea = styled.div`
+  margin: 0 auto;
+  max-width: 900px;
+`;
--- frontend/pages/search.tsx
+++ frontend/pages/search.tsx
@@ -1,7 +1,7 @@
 import GNB from '../components/common/GNB';
-import SearchBar from '../components/Search/SearchBar';
-import SearchFilter from '../components/Search/SearchFilter';
-import { SearchArea } from '../components/Search/styled';
+import SearchBar from '../components/search/SearchBar';
+import SearchFilter from '../components/search/SearchFilter';
+import { SearchArea } from '../components/search/styled';
 
 export default function Search() {
   return (
