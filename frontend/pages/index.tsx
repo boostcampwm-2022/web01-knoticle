fca05ca5778116e14d4a563f30cd2d56ee94a884
dahyeon405 <dahyeon405@naver.com>
2022-11-20 23:42:55 +0900

    feat: 메인페이지 레이아웃 - #22

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,5 +1,15 @@
+import Footer from '../components/common/Footer';
 import GNB from '../components/common/GNB';
+import Slider from '../components/Slider';
+import { FlexColumnCenter } from '../styles/layout';
 
 export default function Home() {
-  return <GNB />;
+  return (
+    <FlexColumnCenter style={{ 'background-color': '#fffce9' }}>
+      <GNB />
+      <Slider />
+      <Slider />
+      <Footer />
+    </FlexColumnCenter>
+  );
 }
