e7601dea1a2173928c0736156d7689b4f63e9b50
dahyeon405 <dahyeon405@naver.com>
2022-11-20 23:42:18 +0900

    feat: Footer 컴포넌트 UI 작업 - #22

---
+++ frontend/components/common/Footer/styled.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+import { FlexCenter } from '../../../styles/layout';
+
+const FooterContent = styled(FlexCenter)`
+  flex-direction: column;
+  padding: 50px;
+  gap: 8px;
+  color: var(--grey-01-color);
+  font-size: 14px;
+`;
+
+export default FooterContent;
