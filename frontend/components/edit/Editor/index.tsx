b9b8f0865565a044f98db364963de9a48f0448f7
김도현 <swputer@gmail.com>
2022-11-22 18:06:54 +0900

    feat: @uiw 에디터 추가 - #63

--- frontend/components/edit/Editor/index.tsx
+++ frontend/components/edit/Editor/index.tsx
@@ -1,5 +1,33 @@
+import dynamic from 'next/dynamic';
+
+import { useEffect, useState } from 'react';
+
 import { EditorWrapper } from './styled';
 
+import '@uiw/react-md-editor/markdown-editor.css';
+import '@uiw/react-markdown-preview/markdown.css';
+
+const MDEditor = dynamic(() => import('@uiw/react-md-editor'), {
+  ssr: false,
+});
+
 export default function Editor() {
-  return <EditorWrapper />;
+  const [content, setContent] = useState<string | undefined>('');
+  const [editorHeight, setEditorHeight] = useState(0);
+
+  useEffect(() => {
+    setEditorHeight(window.innerHeight - 68);
+  }, []);
+
+  return (
+    <EditorWrapper>
+      <MDEditor
+        height={editorHeight}
+        value={content}
+        onChange={(value) => {
+          setContent(value);
+        }}
+      />
+    </EditorWrapper>
+  );
 }
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,12 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const removeImports = require('next-remove-imports')();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 };
 
-module.exports = nextConfig;
+module.exports = removeImports({
+  ...nextConfig,
+});
