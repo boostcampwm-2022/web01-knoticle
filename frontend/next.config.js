550a6ae893ea2eea3efde5b1e55ea500fb6dac66
김도현 <swputer@gmail.com>
2022-11-23 02:44:09 +0900

    chore: 불필요한 패키지 제거

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,12 +1,7 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const removeImports = require('next-remove-imports')();
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 };
 
-module.exports = removeImports({
-  ...nextConfig,
-});
+module.exports = nextConfig;
