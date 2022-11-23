ef4a4b78bc7aaa457d094a98937244b300747c4a
dahyeon405 <dahyeon405@naver.com>
2022-11-23 17:48:27 +0900

    chore: authService의 토큰 관련 함수 token.ts로 분리

--- backend/src/apis/auth/auth.service.ts
+++ backend/src/apis/auth/auth.service.ts
@@ -1,6 +1,5 @@
 import { prisma } from '@config/orm.config';
 import { ResourceConflict, Message, Unauthorized } from '@errors';
-import { generateJWT } from '@utils/token';
 import axios from 'axios';
 import { hash, compare } from 'bcrypt';
 
@@ -22,31 +21,6 @@ const getSignedUser = async (username: string, password: string) => {
   if (!(await compare(password, user.password))) throw new Unauthorized(Message.AUTH_WRONG);
 
   return user;
-};
-
-const getTokens = (userId: number, nickname: string) => {
-  const accessToken = generateJWT('3h', { id: userId, nickname });
-  const refreshToken = generateJWT('7d');
-
-  return {
-    accessToken,
-    refreshToken,
-  };
-};
-
-const saveRefreshToken = async (userId: number, refreshToken: string) => {
-  await prisma.token.upsert({
-    where: {
-      user_id: userId,
-    },
-    update: {
-      refresh_token: refreshToken,
-    },
-    create: {
-      user_id: userId,
-      refresh_token: refreshToken,
-    },
-  });
 };
 
 const getGithubAccessToken = async (code: string) => {
@@ -160,8 +134,6 @@ const signUpLocalUser = async (username: string, password: string, nickname: str
 
 export default {
   getSignedUser,
-  getTokens,
-  saveRefreshToken,
   getGithubAccessToken,
   getUserByGithubAPI,
   getUserByLocalDB,
--- backend/src/utils/token.ts
+++ backend/src/utils/token.ts
@@ -1,12 +1,61 @@
-import jwt from 'jsonwebtoken';
+import { prisma } from '@config/orm.config';
+import jwt, { JwtPayload } from 'jsonwebtoken';
 
-export const generateJWT = (
-  expiresIn: '3h' | '7d',
-  payload: { id?: number; nickname?: string } = {}
-) => {
+const generateJWT = (expiresIn: '3h' | '7d', payload: { id?: number; nickname?: string } = {}) => {
   return jwt.sign(payload, process.env.JWT_SECRET_KEY, { expiresIn });
 };
 
-export const decodeJWT = (token: string) => {
-  return jwt.verify(token, process.env.JWT_SECRET_KEY);
+const decodeJWT = (token: string) => {
+  return jwt.verify(token, process.env.JWT_SECRET_KEY) as JwtPayload;
+};
+
+const getTokens = (userId: number, nickname: string) => {
+  const accessToken = generateJWT('3h', { id: userId, nickname });
+  const refreshToken = generateJWT('7d');
+
+  return {
+    accessToken,
+    refreshToken,
+  };
+};
+
+const saveRefreshToken = async (userId: number, refreshToken: string) => {
+  await prisma.token.upsert({
+    where: {
+      user_id: userId,
+    },
+    update: {
+      refresh_token: refreshToken,
+    },
+    create: {
+      user_id: userId,
+      refresh_token: refreshToken,
+    },
+  });
+};
+
+const checkRefreshTokenValid = async (refresh_token: string) => {
+  const matchedToken = await prisma.token.findFirst({
+    where: {
+      refresh_token,
+    },
+    select: {
+      user_id: true,
+      user: {
+        select: {
+          nickname: true,
+        },
+      },
+    },
+  });
+  if (!matchedToken) return false;
+  return matchedToken;
+};
+
+export default {
+  generateJWT,
+  decodeJWT,
+  getTokens,
+  saveRefreshToken,
+  checkRefreshTokenValid,
 };
