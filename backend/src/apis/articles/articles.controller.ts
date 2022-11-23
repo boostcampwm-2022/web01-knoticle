f84a3a6b49d45be665a720c249cd62569b6c2a46
parkhyeonki <phk147@gmail.com>
2022-11-23 10:18:01 +0900

    chore: 주석 제거 - #44

--- backend/src/apis/articles/articles.controller.ts
+++ backend/src/apis/articles/articles.controller.ts
@@ -6,8 +6,6 @@ const getArticle = async (req: Request, res: Response) => {
   const articleId = Number(req.params.articleId);
   const articleData = await articleService.getArticleData(articleId);
 
-  // 쿠키에서 유저아이디 검출 미들웨어 추가 -> req.userId -> 북마크 확인
-
   res.status(200).send(articleData);
 };
 
