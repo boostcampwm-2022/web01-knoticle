60c924a9ca7612d7b0415b3445f5672bea6d1a18
김도현 <swputer@gmail.com>
2022-11-24 16:58:13 +0900

    feat: Preview 컴포넌트를 Content 컴포넌트로 변경 - #98

---
+++ frontend/components/common/Content/styled.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const ContentWrapper = styled.div`
+  width: 100%;
+`;
+
+export const ContentTitle = styled.h1`
+  font-size: 36px;
+  font-weight: 700;
+`;
+
+export const ContentBody = styled.div``;
