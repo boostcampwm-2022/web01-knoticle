131df701ddddfa9ccbec156d7581809f78c73448
dahyeon405 <dahyeon405@naver.com>
2022-11-24 13:47:00 +0900

    feat: bookmarkApi 추가 - #38, #55

---
+++ frontend/apis/bookmarkApi.ts
@@ -0,0 +1,33 @@
+import api from '@utils/api';
+
+interface PostBookmarkApi {
+  book_id: number;
+}
+
+interface DeleteBookmarkApi {
+  bookmarkId: number;
+}
+
+// eslint-disable-next-line import/prefer-default-export
+export const postBookmarkApi = async (data: PostBookmarkApi) => {
+  const url = `/api/bookmarks`;
+
+  const response = await api({
+    url,
+    method: 'POST',
+    data,
+  });
+
+  return response.data;
+};
+
+export const deleteBookmarkApi = async (data: DeleteBookmarkApi) => {
+  const url = `/api/bookmarks/${data.bookmarkId}`;
+
+  const response = await api({
+    url,
+    method: 'DELETE',
+  });
+
+  return true;
+};
