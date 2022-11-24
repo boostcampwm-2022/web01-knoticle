a50fbc5a6b8eacfd7c173849ae8024dcc7fe30d9
MinHK4 <dzf1224@naver.com>
2022-11-24 13:58:07 +0900

    feat: 메인 페이지 책 API 요청 - 51

--- frontend/apis/bookApi.ts
+++ frontend/apis/bookApi.ts
@@ -4,9 +4,20 @@ interface GetBooksApi {
   userId: string;
 }
 
-// eslint-disable-next-line import/prefer-default-export
 export const getBooksApi = async (data: GetBooksApi) => {
   const url = `/api/books?user=${data.userId}`;
+
+  const response = await api({ url, method: 'GET' });
+
+  return response.data;
+};
+
+interface GetOrderedBookListApi {
+  order: string;
+}
+
+export const getOrderedBookListApi = async (data: GetOrderedBookListApi) => {
+  const url = `/api/books?order=${data.order}`;
 
   const response = await api({ url, method: 'GET' });
 
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,9 +1,21 @@
+import { useEffect } from 'react';
+
+import { getOrderedBookListApi } from '@apis/bookApi';
 import Footer from '@components/common/Footer';
 import GNB from '@components/common/GNB';
 import Slider from '@components/home/Slider';
+import useFetch from '@hooks/useFetch';
 import { PageInnerLarge, PageWrapper } from '@styles/layout';
 
 export default function Home() {
+  const { data: newestBookList, execute: getNewestBookList } = useFetch(getOrderedBookListApi);
+  const { data: popularBookList, execute: getPopularBookList } = useFetch(getOrderedBookListApi);
+
+  useEffect(() => {
+    getNewestBookList('newest');
+    getPopularBookList('bookmark');
+  }, []);
+
   return (
     <>
       <GNB />
