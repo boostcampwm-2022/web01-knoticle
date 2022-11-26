c617825c891f366e6688f852a352d0e43853def7
김도현 <swputer@gmail.com>
2022-11-27 01:57:27 +0900

    feat: 에디터 이미지 붙여넣기 이벤트 핸들러 추가 - #64

--- frontend/components/edit/Editor/index.tsx
+++ frontend/components/edit/Editor/index.tsx
@@ -6,14 +6,17 @@ import { markdown, markdownLanguage } from '@codemirror/lang-markdown';
 import { languages } from '@codemirror/language-data';
 import { tags } from '@lezer/highlight';
 import { createTheme } from '@uiw/codemirror-themes';
+import axios from 'axios';
 import { useRecoilState } from 'recoil';
 import rehypeStringify from 'rehype-stringify';
 import remarkParse from 'remark-parse';
 import remarkRehype from 'remark-rehype';
 import { unified } from 'unified';
 
+import { createImageApi } from '@apis/imageApi';
 import articleState from '@atoms/article';
 import Content from '@components/common/Content';
+import useFetch from '@hooks/useFetch';
 import useInput from '@hooks/useInput';
 
 import { CodeMirrorWrapper, EditorInner, EditorWrapper, TitleInput } from './styled';
@@ -23,35 +26,14 @@ const CodeMirror = dynamic(() => import('@uiw/react-codemirror'), {
 });
 
 export default function Editor() {
+  const { data: imagePath, execute: createImage } = useFetch(createImageApi);
+
   const [article, setArticle] = useRecoilState(articleState);
 
   const [content, setContent] = useState('');
   const [height, setHeight] = useState(0);
 
   const title = useInput();
-
-  useEffect(() => {
-    setHeight(window.innerHeight - 68);
-  }, []);
-
-  useEffect(() => {
-    setArticle({
-      ...article,
-      title: title.value,
-    });
-  }, [title.value]);
-
-  useEffect(() => {
-    setArticle({
-      ...article,
-      content: unified()
-        .use(remarkParse)
-        .use(remarkRehype)
-        .use(rehypeStringify)
-        .processSync(content)
-        .toString(),
-    });
-  }, [content]);
 
   const theme = createTheme({
     theme: 'light',
@@ -79,6 +61,52 @@ export default function Editor() {
     ],
   });
 
+  useEffect(() => {
+    setHeight(window.innerHeight - 68);
+  }, []);
+
+  useEffect(() => {
+    setArticle({
+      ...article,
+      title: title.value,
+    });
+  }, [title.value]);
+
+  useEffect(() => {
+    setArticle({
+      ...article,
+      content: unified()
+        .use(remarkParse)
+        .use(remarkRehype)
+        .use(rehypeStringify)
+        .processSync(content)
+        .toString(),
+    });
+  }, [content]);
+
+  useEffect(() => {
+    console.log(imagePath);
+  }, [imagePath]);
+
+  const handleImagePaste = (event: ClipboardEvent) => {
+    if (!event.clipboardData) return;
+
+    const { items } = event.clipboardData;
+
+    // eslint-disable-next-line no-restricted-syntax
+    for (const item of items) {
+      if (item.kind === 'file' && /image\/[png,jpg,jpeg,gif]/.test(item.type)) {
+        const blob = item.getAsFile();
+
+        const formData = new FormData();
+
+        formData.append('image', blob);
+
+        createImage(formData);
+      }
+    }
+  };
+
   return (
     <EditorWrapper style={{ height }}>
       <EditorInner>
@@ -101,6 +129,7 @@ export default function Editor() {
               highlightActiveLine: false,
             }}
             placeholder="내용을 입력해주세요"
+            onPasteCapture={(event) => handleImagePaste(event)}
           />
         </CodeMirrorWrapper>
       </EditorInner>
