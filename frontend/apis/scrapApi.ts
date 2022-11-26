e9dd40ae2896d97e83e527052b2183680b231057
dahyeon405 <dahyeon405@naver.com>
2022-11-26 22:45:44 +0900

    feat: createScrapApi 추가(FE) - #70

---
+++ frontend/apis/scrapApi.ts
@@ -0,0 +1,17 @@
+import api from '@utils/api';
+
+interface CreateScrapApi {
+  order: number;
+  is_original: boolean;
+  book_id: number;
+  article_id: number;
+}
+
+// eslint-disable-next-line import/prefer-default-export
+export const createScrapApi = async (data: CreateScrapApi) => {
+  const url = `/api/scraps`;
+
+  const response = await api({ url, method: 'POST', data });
+
+  return response.data;
+};
