77c15627580e9a5575f52e1fced5cc73c0414f58
dahyeon405 <dahyeon405@naver.com>
2022-11-27 23:39:11 +0900

    feat: 스크랩되어 있는지 여부 체크하는 로직 추가(BE) - #71

--- backend/src/apis/scraps/scraps.controller.ts
+++ backend/src/apis/scraps/scraps.controller.ts
@@ -3,6 +3,8 @@ import { Request, Response } from 'express';
 import scrapsService from './scraps.service';
 
 const createScrap = async (req: Request, res: Response) => {
+  await scrapsService.checkScrapExists(req.body);
+
   const scrap = await scrapsService.createScrap(req.body);
 
   res.status(201).send({ scrap });
