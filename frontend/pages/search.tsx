189111a390b74e939e9a6e86c919ceb5d7a19d35
dahyeon405 <dahyeon405@naver.com>
2022-11-29 16:45:53 +0900

    feat: 검색바에 useDebounce 훅 적용 - #115

--- frontend/pages/search.tsx
+++ frontend/pages/search.tsx
@@ -1,18 +1,32 @@
+import { useEffect, useState } from 'react';
+
 import GNB from '@components/common/GNB';
 import SearchBar from '@components/search/SearchBar';
 import SearchFilter from '@components/search/SearchFilter';
 import SearchListItem from '@components/search/SearchListItem';
+import useDebounce from '@hooks/useDebounce';
 import { PageInnerSmall, PageWrapper } from '@styles/layout';
 
 export default function Search() {
   const items = Array.from({ length: 50 }, () => 0);
+
+  const [keyword, setKeyword] = useState('');
+  const debouncedKeyword = useDebounce(keyword, 1000);
+
+  const handleSearchbarOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setKeyword(e.target.value);
+  };
+
+  useEffect(() => {
+    // 데이터 받아오기
+  }, [debouncedKeyword]);
 
   return (
     <>
       <GNB />
       <PageWrapper>
         <PageInnerSmall>
-          <SearchBar />
+          <SearchBar handleSearchbarOnChange={handleSearchbarOnChange} />
           <SearchFilter />
           <div>
             {items.map((item) => (
