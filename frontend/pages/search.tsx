bba5488b40faf7ce80baf6f255752c18053d278d
김도현 <swputer@gmail.com>
2022-11-29 16:38:23 +0900

    feat: 글 검색 요청 로직 추가 - #116

--- frontend/pages/search.tsx
+++ frontend/pages/search.tsx
@@ -1,11 +1,15 @@
+import { searchArticlesApi } from '@apis/articleApi';
 import GNB from '@components/common/GNB';
 import SearchBar from '@components/search/SearchBar';
 import SearchFilter from '@components/search/SearchFilter';
 import SearchListItem from '@components/search/SearchListItem';
+import useFetch from '@hooks/useFetch';
 import { PageInnerSmall, PageWrapper } from '@styles/layout';
 
 export default function Search() {
   const items = Array.from({ length: 50 }, () => 0);
+
+  const { data: articles, execute: searchArticles } = useFetch(searchArticlesApi);
 
   return (
     <>
