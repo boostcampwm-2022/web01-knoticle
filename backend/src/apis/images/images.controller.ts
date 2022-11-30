27b1dab78330682a556c56e5cb4656e9dd18a5b8
parkhyeonki <phk147@gmail.com>
2022-11-30 15:38:17 +0900

    feat: 책 수정 데이터 로직 - #135

--- backend/src/apis/images/images.controller.ts
+++ backend/src/apis/images/images.controller.ts
@@ -3,6 +3,7 @@ import { Request, Response } from 'express';
 import imagesService from '@apis/images/images.service';
 
 const createImage = async (req: Request, res: Response) => {
+  console.log(req.file);
   const imagePath = await imagesService.createImage({ file: req.file });
 
   res.status(201).send({ imagePath });
--- frontend/components/study/EditBook/index.tsx
+++ frontend/components/study/EditBook/index.tsx
@@ -1,8 +1,14 @@
 import Image from 'next/image';
 
+import { useEffect, useRef, useState } from 'react';
+
+import { createImageApi } from '@apis/imageApi';
+import Edit from '@assets/ico_edit.svg';
 import MoreContentsIcon from '@assets/ico_more_contents.svg';
 import SampleThumbnail from '@assets/img_sample_thumbnail.jpg';
 import Button from '@components/common/Modal/ModalButton';
+import useFetch from '@hooks/useFetch';
+import useInput from '@hooks/useInput';
 import { IBookScraps } from '@interfaces';
 import { FlexCenter, FlexSpaceBetween } from '@styles/layout';
 
@@ -17,6 +23,9 @@ import {
   Author,
   Input,
   BookContent,
+  EditBookWapper,
+  EditBookThumbnailWrapper,
+  EditBookThumbnailIcon,
 } from './styled';
 
 interface BookProps {
@@ -25,20 +34,55 @@ interface BookProps {
 
 export default function EditBook({ book }: BookProps) {
   const { id, title, user, scraps } = book;
+  const { value: titleData, onChange: onTitleChange } = useInput(title);
+  const { data: imgFile, execute: createImage } = useFetch(createImageApi);
+  const [scrapList, setScrapList] = useState(scraps);
+
+  const inputFile = useRef<HTMLInputElement | null>(null);
+
+  const handleEditBookImgClick = () => {
+    if (!inputFile.current) return;
+    inputFile.current.click();
+  };
+
+  const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.stopPropagation();
+    event.preventDefault();
+
+    if (!event.target.files) return;
+
+    const formData = new FormData();
+    formData.append('image', event.target.files[0]);
+    createImage(formData);
+  };
+
+  const handleCompletedBtnClick = () => {
+    console.log(id);
+    console.log(titleData);
+    console.log('click', imgFile);
+    console.log(scrapList);
+  };
 
   return (
-    <>
+    <EditBookWapper>
       <BookWrapper>
-        <BookThumbnail src={SampleThumbnail} alt="thumbnail" />
-        {/* 수정 버튼 추가 예정 */}
+        <EditBookThumbnailWrapper>
+          <BookThumbnail src={SampleThumbnail} alt="thumbnail" />
+          <EditBookThumbnailIcon onClick={handleEditBookImgClick}>
+            <Image src={Edit} alt="profile_edit" width={20} />
+            <input
+              type="file"
+              id="file"
+              ref={inputFile}
+              style={{ display: 'none' }}
+              onChange={handleFileUpload}
+            />
+          </EditBookThumbnailIcon>
+        </EditBookThumbnailWrapper>
         <BookInfoContainer>
           <FlexSpaceBetween>
             <BookTitle>
-              <Input
-                type="text"
-                defaultValue={title}
-                onChange={(e) => console.log(e.target.value)}
-              />
+              <Input type="text" defaultValue={title} onChange={onTitleChange} />
               <Author>by {user.nickname}</Author>
             </BookTitle>
           </FlexSpaceBetween>
@@ -61,9 +105,9 @@ export default function EditBook({ book }: BookProps) {
           </FlexCenter>
         </BookInfoContainer>
       </BookWrapper>
-      <Button theme="primary" onClick={() => console.log('수정!')}>
+      <Button theme="primary" onClick={handleCompletedBtnClick}>
         수정 완료
       </Button>
-    </>
+    </EditBookWapper>
   );
 }
