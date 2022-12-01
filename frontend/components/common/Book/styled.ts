5a3d5518094932c96172f4b49f309516a2475abb
MinHK4 <dzf1224@naver.com>
2022-12-01 15:54:10 +0900

    chore: 북 내부 사이즈 수정

--- frontend/components/common/Book/styled.ts
+++ frontend/components/common/Book/styled.ts
@@ -8,7 +8,7 @@ import { FlexColumn } from '@styles/layout';
 
 export const BookWrapper = styled(FlexColumn)`
   min-width: 280px;
-  min-height: 480px;
+  height: 480px;
   margin: 0 10px;
   box-sizing: border-box;
 
@@ -27,7 +27,7 @@ export const BookThumbnail = styled(Image)`
 `;
 
 export const BookInfoContainer = styled(FlexColumn)`
-  padding: 24px;
+  padding: 15px 24px;
   gap: 18px;
 `;
 
@@ -50,6 +50,7 @@ export const BookContentsInfo = styled(FlexColumn)`
   gap: 8px;
 
   div:nth-child(1) {
+    box-sizing: border-box;
     padding-bottom: 10px;
     border-bottom: 1px solid var(--primary-color);
   }
@@ -58,6 +59,7 @@ export const BookContentsInfo = styled(FlexColumn)`
 export const BookContents = styled(TextXSmall)`
   display: flex;
   flex-direction: column;
+  box-sizing: border-box;
 
   a {
     border-bottom: 1px solid var(--grey-02-color);
