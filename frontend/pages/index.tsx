751eeeb6cfa1adda19fd729e08405a6d04b816c1
MinHK4 <dzf1224@naver.com>
2022-12-06 18:51:29 +0900

    feat: 메인페이지 isLoading 상태 추가 - #216

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import { getOrderedBookListApi } from '@apis/bookApi';
 import Footer from '@components/common/Footer';
@@ -12,10 +12,18 @@ export default function Home() {
   const { data: newestBookList, execute: getNewestBookList } = useFetch(getOrderedBookListApi);
   const { data: popularBookList, execute: getPopularBookList } = useFetch(getOrderedBookListApi);
 
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
   useEffect(() => {
     getNewestBookList('newest');
     getPopularBookList('bookmark');
   }, []);
+
+  useEffect(() => {
+    if (!newestBookList || !popularBookList) return;
+
+    setIsLoading(false);
+  }, [newestBookList, popularBookList]);
 
   return (
     <>
@@ -23,8 +31,8 @@ export default function Home() {
       <GNB />
       <PageWrapper>
         <PageInnerLarge>
-          {newestBookList && <Slider bookList={newestBookList} title="새로 엮은 책" />}
-          {popularBookList && <Slider bookList={popularBookList} title="가장 인기 있는 책" />}
+          <Slider bookList={newestBookList} title="새로 엮은 책" isLoading={isLoading} />
+          <Slider bookList={popularBookList} title="가장 인기 있는 책" isLoading={isLoading} />
           <Footer />
         </PageInnerLarge>
       </PageWrapper>
