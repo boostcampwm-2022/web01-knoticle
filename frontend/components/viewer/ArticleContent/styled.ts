baf118bbb07ffad9ac24f6a1811f7fe156b6ab81
dahyeon405 <dahyeon405@naver.com>
2022-12-08 14:37:35 +0900

    feat: 뷰어페이지 아티클 이동 버튼 반응형 적용 - #250

--- frontend/components/viewer/ArticleContent/styled.ts
+++ frontend/components/viewer/ArticleContent/styled.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-import { Flex } from '@styles/layout';
+import { Flex, FlexColumn } from '@styles/layout';
 
 export const ArticleContainer = styled(Flex)`
   flex: 1;
@@ -11,27 +11,56 @@ export const ArticleLeftBtn = styled.div`
   top: 50%;
   margin-left: 20px;
   cursor: pointer;
+
+  @media ${(props) => props.theme.mobile} {
+    top: 97px;
+  }
 `;
 export const ArticleRightBtn = styled.div`
   position: fixed;
   top: 50%;
-  right: 25px;
+  right: 25px;s
   cursor: pointer;
+
+  @media ${(props) => props.theme.mobile} {
+    top: 97px;
+  }
 `;
 export const ArticleMain = styled(Flex)`
   flex-direction: column;
   width: 100%;
   padding: 50px;
-  overflow: auto;
+  overflow-y: scroll;
+  box-sizing: border-box;
+
+  ::-webkit-scrollbar {
+    width: 10px;
+  }
+  ::-webkit-scrollbar-thumb {
+    background-color: var(--grey-02-color);
+    border-radius: 10px;
+  }
+
+  @media ${(props) => props.theme.mobile} {
+    padding: 50px 16px;
+  }
 `;
-export const ArticleTitle = styled(Flex)`
+export const ArticleTitle = styled.div`
   width: 100%;
   border-bottom: 1px solid black;
   padding: 25px 0;
-  justify-content: space-between;
+  text-align: left;
 `;
-export const ArticleTitleBtnBox = styled(Flex)``;
+export const ArticleTitleBtnBox = styled(Flex)`
+  flex-wrap: wrap;
+  gap: 8px;
+  margin-top: 16px;
+  border-top: 1px solid var(--grey-02-color);
+  padding-top: 10px;
+`;
 export const ArticleContents = styled.div`
   margin-top: 20px;
   height: 481px;
 `;
+
+export const ArticleContentsWrapper = styled(FlexColumn)``;
