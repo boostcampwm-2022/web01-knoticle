4d7018231bbf4205f66a479ad86986a800b53015
김도현 <swputer@gmail.com>
2022-12-10 01:46:22 +0900

    feat: 글 검색 제목 말줄임 추가

--- frontend/components/search/ArticleItem/styled.ts
+++ frontend/components/search/ArticleItem/styled.ts
@@ -22,6 +22,11 @@ export const ItemGroup = styled.div`
 
 export const ItemTitle = styled(TextMedium)`
   margin-bottom: 16px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 1;
+  -webkit-box-orient: vertical;
 `;
 
 export const ItemContent = styled(TextSmall)`
