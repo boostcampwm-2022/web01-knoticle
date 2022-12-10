19d4d2f6230585282e6a7510be26237f1b8f5566
dahyeon405 <dahyeon405@naver.com>
2022-12-11 04:00:56 +0900

    feat: Footer에 github 링크 추가

--- frontend/components/common/Footer/index.tsx
+++ frontend/components/common/Footer/index.tsx
@@ -1,11 +1,13 @@
-import FooterContent from './styled';
+import { FooterContent, FooterLink } from './styled';
 
 function Footer() {
   return (
     <footer>
       <FooterContent>
         <div>Copyright © 2022 by Knoticle Team</div>
-        <div>Github</div>
+        <div>
+          <FooterLink href="https://github.com/boostcampwm-2022/web01-knoticle">Github</FooterLink>
+        </div>
       </FooterContent>
     </footer>
   );
