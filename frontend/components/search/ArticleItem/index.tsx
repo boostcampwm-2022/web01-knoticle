67743b72eda5685480659bb02bfda1bc6849192c
dahyeon405 <dahyeon405@naver.com>
2022-12-12 00:35:28 +0900

    feat: 검색 결과, 필터링 정보에 useSessionStorage 훅 적용 - #223

--- frontend/components/search/ArticleItem/index.tsx
+++ frontend/components/search/ArticleItem/index.tsx
@@ -15,8 +15,8 @@ import {
 } from './styled';
 
 interface ArticleItemProps {
-  title: string;
-  content: string;
+  title: React.ReactNode;
+  content: React.ReactNode;
   nickname: string;
   articleUrl: string;
   studyUrl: string;
--- frontend/components/search/ArticleList/index.tsx
+++ frontend/components/search/ArticleList/index.tsx
@@ -3,16 +3,48 @@ import { IArticleBook } from '@interfaces';
 
 interface ArticleListProps {
   articles: IArticleBook[];
+  keywords: string[];
 }
 
-export default function ArticleList({ articles }: ArticleListProps) {
+export default function ArticleList({ articles, keywords }: ArticleListProps) {
+  const highlightWord = (text: string, words: string[], isFirst = false): React.ReactNode => {
+    let wordIndexList = words.map((word) => text.toLowerCase().indexOf(word.toLowerCase()));
+
+    const filteredWords = words.filter((_, index) => wordIndexList[index] !== -1);
+    wordIndexList = wordIndexList.filter((wordIndex) => wordIndex !== -1);
+
+    if (wordIndexList.length === 0) return text;
+
+    const startIndex = Math.min(...wordIndexList);
+
+    const targetWord = filteredWords[wordIndexList.indexOf(startIndex)];
+
+    const endIndex = startIndex + targetWord.length;
+
+    let paddingIndex = 0;
+
+    if (isFirst) {
+      const regex = /(<([^>]+)>)/g;
+
+      while (regex.test(text.slice(0, startIndex))) paddingIndex = regex.lastIndex;
+    }
+
+    return (
+      <>
+        {text.slice(paddingIndex, startIndex)}
+        <b>{text.slice(startIndex, endIndex)}</b>
+        {highlightWord(text.slice(endIndex).replace(/(<([^>]+)>)/gi, ''), words)}
+      </>
+    );
+  };
+
   return (
     <>
       {articles.map((article) => (
         <ArticleItem
           key={article.id}
-          title={article.title}
-          content={article.content}
+          title={highlightWord(article.title, keywords)}
+          content={highlightWord(article.content, keywords, true)}
           nickname={article.book.user.nickname}
           articleUrl={`/viewer/${article.book.id}/${article.id}`}
           studyUrl={`/study/${article.book.user.nickname}`}
